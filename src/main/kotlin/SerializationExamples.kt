8cc22ec3175e88506e03cb78b5c182015ed928d6
Konstantin <vpor901.gmail.com>
2025-09-13 22:04:16 +1100

    add-json-key

--- src/main/kotlin/SerializationExamples.kt
+++ src/main/kotlin/SerializationExamples.kt
@@ -1,8 +1,52 @@
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+
+@Serializable
+data class Update(
+    val update_id: Long,
+)
+
+@Serializable
+data class Response(
+    val result: List<Update>,
+)
 
 fun main() {
 
-    val word = Json.encodeToString(
+    val json = Json {
+        ignoreUnknownKeys = true
+    }
+
+    val responseString = """
+        {
+           "ok":true,
+           "result":[
+              {
+                 "update_id":708547862,
+                 "message":{
+                    "message_id":292,
+                    "from":{
+                       "id":1534297299,
+                       "is_bot":false,
+                       "first_name":"Kot",
+                       "username":"sobakakott",
+                       "language_code":"ru"
+                    },
+                    "chat":{
+                       "id":1534297299,
+                       "first_name":"Kot",
+                       "username":"sobakakott",
+                       "type":"private"
+                    },
+                    "date":1757760444,
+                    "text":"start"
+                 }
+              }
+           ]
+        }
+    """.trimIndent()
+
+    /*val word = Json.encodeToString(
         Word(
             original = "Hello",
             translate = "Привет",
@@ -14,6 +58,9 @@ fun main() {
     val wordObject = Json.decodeFromString<Word>(
         """{"original": "Hello","translate": "привет"}"""
     )
-    println(wordObject)
+    println(wordObject)*/
+
+    val response = json.decodeFromString<Response>(responseString)
+    println(response)
     
 }
