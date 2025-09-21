f42fec2e6187775ab7fdc9179d4d0b018e3eee3d
Konstantin <vpor901.gmail.com>
2025-09-21 18:47:18 +1100

    add-github-workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.20"
     kotlin("plugin.serialization") version "2.2.20"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     application
 }
 
@@ -21,4 +22,8 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(22)
+}
+
+application {
+    mainClass.set("TelegramKt")
 }
