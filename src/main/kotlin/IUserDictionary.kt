ac3bcc7ee9722cd75a7487ab78f46bb900672a39
Konstantin <vpor901.gmail.com>
2025-09-29 20:23:07 +1100

    add-IUserDictionary

---
+++ src/main/kotlin/IUserDictionary.kt
@@ -0,0 +1,8 @@
+interface IUserDictionary {
+    fun getNumOfLearnedWords(): Int
+    fun getSize(): Int
+    fun getLearnedWords(): List<Word>
+    fun getUnlearnedWords(): List<Word>
+    fun setCorrectAnswersCount(word: String, correctAnswersCount: Int)
+    fun resetUserProgress()
+}
