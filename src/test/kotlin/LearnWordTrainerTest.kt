f5c51e860ad6d794d5713839348bab030f8c6152
Konstantin <vpor901.gmail.com>
2025-10-02 19:34:46 +1100

    add-statistics_corrupted

--- src/test/kotlin/LearnWordTrainerTest.kt
+++ src/test/kotlin/LearnWordTrainerTest.kt
@@ -1,4 +1,23 @@
-import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
 class LearnWordTrainerTest {
-  
+
+    @Test
+    fun `test statistics with 4 words of 7`()
+    {
+        val trainer = LearnWordTrainer("src/test/4_word_of_7.txt")
+        kotlin.test.assertEquals(
+            Statistics(learnedCount = 4, totalCount = 7, percentCount = 57),
+            trainer.getStatistics()
+        )
+    }
+
+    @Test
+    fun `test statistics with corrupted file`() {
+        val trainer = LearnWordTrainer("src/test/corrupted_file.txt")
+        kotlin.test.assertEquals(
+            Statistics(learnedCount = 0, totalCount = 0, percentCount = 0),
+            trainer.getStatistics()
+        )
+    }
 }
