d81a96e8d06ddd2e6ac2325ea3965ace3f44123f
Konstantin <vpor901.gmail.com>
2025-10-02 19:39:25 +1100

    add-testNextQuestion

--- src/test/kotlin/LearnWordTrainerTest.kt
+++ src/test/kotlin/LearnWordTrainerTest.kt
@@ -20,4 +20,34 @@ class LearnWordTrainerTest {
             trainer.getStatistics()
         )
     }
+
+    @Test
+    fun `test getNextQuestion with 5 unlearned words`() {
+        val trainer = LearnWordTrainer("src/test/5_unlearned_words.txt")
+        val question = trainer.getNextQuestion()
+
+        kotlin.test.assertNotNull(question, "Вопрос не должен быть null")
+        kotlin.test.assertTrue(question.variants.size >= 4, "Должно быть минимум 4 варианта ответа")
+        kotlin.test.assertTrue(question.variants.contains(question.correctAnswer),
+            "Варианты должны содержать правильный ответ")
+    }
+
+    @Test
+    fun `test getNextQuestion with 1 unlearned word`() {
+        val trainer = LearnWordTrainer("src/test/1_unlearned_word.txt")
+        val question = trainer.getNextQuestion()
+
+        kotlin.test.assertNotNull(question, "Вопрос не должен быть null")
+        kotlin.test.assertTrue(question.variants.size >= 1, "Должен быть минимум 1 вариант ответа")
+        kotlin.test.assertEquals(question.variants[0], question.correctAnswer,
+            "Единственный вариант должен быть правильным ответом")
+    }
+
+    @Test
+    fun `test getNextQuestion with all words learned`() {
+        val trainer = LearnWordTrainer("src/test/all_words_learned.txt")
+        val question = trainer.getNextQuestion()
+
+        kotlin.test.assertNull(question, "Вопрос должен быть null, когда все слова изучены")
+    }
 }
